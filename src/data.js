cafe57fa4f5496f392c8d7a5b3f28c1b072f87ab
mehruddin khan <77490569+mehruddin11@users.noreply.github.com>
2021-07-10 20:48:58 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ const reviews = [
   {
     id: 1,
     name: 'mehruddin',
-    job: 'Oggy and the Cocroaches',
+    job: 'Web Developer',
     image:Image,
     text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris'
   },
